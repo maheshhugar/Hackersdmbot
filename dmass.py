c86d1a3be5727b324fe7e996d3369a6b43b96eac
maheshhugar <54721452+maheshhugar@users.noreply.github.com>
2019-10-18 23:04:29 +0530

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("NjA4MDcxNTEyMTg3OTk0MTQy.XXHE4w.7x4jawjHJYWctmaWbBa_5pMN484")                
+client.run("NjM0ODA1MTQ1NjUzNDc3Mzg4.Xan2ig.jBkb7ziMYvbvh95-gYnNiws03wo")           
